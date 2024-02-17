619639538477e3514131f80e9f47f7419f55debc
SebastianOpiyo <sebamopale@gmail.com>
2024-02-17 21:27:48 +0300

    Add apis and models for custumers and orders

---
+++ customers/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    email = models.EmailField()
+    phone = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
---
+++ customers/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Customer
+from .serializers import CustomerSerializer
+
+class CustomerListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+class CustomerRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
